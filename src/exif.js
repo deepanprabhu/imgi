bc90e5ae3c3c110e921110642adb8d1da0254b9e
Deepan Prabhu Babu <deepan18@gmail.com>
2016-12-18 07:47:08 -0800

    Removing old file handling - EXIF

--- src/exif.js
+++ src/exif.js
@@ -32,39 +32,6 @@ function convertToHexArray(array){
 	return a;
 }
 
-function applyChunk(blob, applyFunction){
-	var reader = new FileReader();
-    reader.onloadend = function(evt){
-		if(evt.target.readyState == FileReader.DONE){
-			applyFunction(evt.target.result);
-		}
-	};
-	reader.readAsArrayBuffer(blob);
-}
-
-EXIF.isFileValid = false;
-
-EXIF.validate = function(file,fileSize){
-
-	//	Verify SOI Marker, FFd8
-	var blob = file.slice(0,2);
-	applyChunk(blob, function(e){
-		if(concatHexArrayAsString(convertToHexArray(e)) != 'ffd8')
-			EXIF.isFileValid = false;
-	});
-
-	//	Verify EOI Marker, FFd9
-	blob = file.slice(fileSize - 2);
-	applyChunk(blob, function(e){
-		if(concatHexArrayAsString(convertToHexArray(e)) != 'ffd8'){
-			EXIF.isFileValid = false;
-		}
-	});
-
-	EXIF.isFileValid = true;
-	return EXIF.isFileValid;
-}
-
 //	DataView, from exif-js. Excellent file handling
 
 EXIF.validateEXIF = function(file, fileSize){
